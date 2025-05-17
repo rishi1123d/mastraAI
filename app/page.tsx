4be744c7b53519d1ccce15dedef056e3866be69e
rishi1123d <rishi.kanaparti@gmail.com>
2025-05-17 02:45:39 -0400

    app layout

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { MeetingMemoryApp } from "@/components/meeting-memory-app"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+      <MeetingMemoryApp />
+    </div>
+  )
+}
